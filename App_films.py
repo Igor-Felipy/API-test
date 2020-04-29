72a68c74c77a285c993ac7cc001fa2e28ed0262f
Igor-Felipy <igor.felipy123@gmail.com>
2020-04-29 19:16:53 -0400

    teste de mostra de dados

--- App_films.py
+++ App_films.py
@@ -1,3 +1,10 @@
 import requests
-r = requests.get('http://www.omdbapi.com/?apikey=[b0b71888]&')
-print(r.json())
+while True:
+    filme = input('Digite o nome de um filme: ')
+    r = requests.get(f'http://www.omdbapi.com/?apikey=dac9a156&t={filme}')
+    dados = r.json()
+    print(dados)
+    print('Ano: {}'.format(dados['Year']))
+    print('Tempo de filme: {}'.format(dados['Runtime']))
+    print('Nota no MetaCritic: {}'.format(dados['Metascore']))
+    input('Pressione <enter> para continuar!')
