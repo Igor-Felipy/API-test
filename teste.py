f7a7b5a5f111b6a332272503b68b03ba5cbf398a
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-11 13:10:37 -0400

    todas as requisições http

--- teste.py
+++ teste.py
@@ -1,2 +1,8 @@
 import requests
-request = requests.get("")
+#tipos de  requisição http:
+r1 = requests.get("")
+r2 = requests.post("")
+r3 = requests.put("")
+r4 = requests.delete("")
+r5 = requests.head("")
+r6 = requests.options("")
