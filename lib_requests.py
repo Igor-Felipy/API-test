5f3646205d0dffe325127a3192996b35425f14fb
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-13 19:33:04 -0400

    solicitação get

--- lib_requests.py
+++ lib_requests.py
@@ -1,7 +1,12 @@
 import requests
 #tipos de  requisição http:
 r1 = requests.get("")#solicita
-r2 = requests.post("")#cria
+r1.text
+r1.json
+r1.content
+
+r2 = requests.post("end point", data={'key':'value'})#cria
+
 r3 = requests.put("")#atualiza/substitui
 r4 = requests.delete("") #apaga
 r5 = requests.head("")#solicita apenas o cabeçalho
